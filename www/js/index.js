20c8fce7e28164c9e48f9aaa3800b94f42d57c15
ZHOQ <32363066+ZHOQ@users.noreply.github.com>
2018-04-20 12:59:44 +0100

    update click

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@
 $(document).on('pageinit', function() {
 	
 	//set up listener for button click
-	$(document).on('click', getPosition);
+	$(document).on("click", "#getLocationButton", getPosition);
 	
 	
 });
